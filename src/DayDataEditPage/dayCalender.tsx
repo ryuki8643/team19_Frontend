ffecdb6356fa9ebf6fd5dc46757105a5f1e2a7a2
yuarth <84562203+yuarth@users.noreply.github.com>
2022-06-21 13:32:25 +0900

    fix:add validation and form to be component

---
+++ src/DayDataEditPage/dayCalender.tsx
@@ -0,0 +1,99 @@
+import FullCalendar, { EventSourceInput, createRef, EventApi, DateSelectArg } from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import allLocales from "@fullcalendar/core/locales-all";
+import { useEffect, useState, useCallback } from "react";
+import { exampleDayDataType } from "../ExampleData/ExampleDataType";
+import Grid from '@mui/material/Grid';
+import EventForm from "./eventForm";
+
+
+export type calendarEventsType = {
+    id: string,
+    title: string,
+    start: string,
+    end: string,
+    memo: string
+    color: string
+}
+
+const DayCalender = () => {
+    const [dailyItems, setDayEventsData] = useState({ userId: "", day: "", content: {} } as exampleDayDataType)
+    let items: calendarEventsType[] = [];
+    const [dayEvents, setDayEvents] = useState([] as calendarEventsType[])
+    const addItems = (dailyEventData: exampleDayDataType) => {
+        if (Object.keys(dailyEventData).length > 0) {
+
+            const data = Object.values(dailyEventData.content)
+            data.forEach(e => {
+                items.push(
+                    {
+                        id: dailyEventData.day + " " + e.eventStart,
+                        start: dailyEventData.day + " " + e.eventStart,
+                        end: dailyEventData.day + " " + e.eventEnd,
+                        title: e.eventName,
+                        memo: e.eventDescription,
+                        color: "3788D8"
+                    }
+                );
+            });
+            setDayEvents(items)
+            setDayEventsData(dailyEventData)
+        }
+    }
+
+    const removeEvent = (title: string, id: string) => {
+        console.log(id)
+        Object.keys(dailyItems.content).map(value => {
+            if (dailyItems.content[Number(value)].eventName === title && (dailyItems.day + " " + dailyItems.content[Number(value)].eventStart) === id) {
+                delete dailyItems.content[Number(value)];
+            }
+        })
+
+        console.log(dailyItems.content)
+        //setDayEventsData()
+    }
+    return (
+        <>
+            <Grid
+                container
+                direction="row"
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Grid >
+                    <EventForm
+                        dailyItems={dailyItems}
+                        addItems={addItems}
+                    />
+                </Grid>
+                <Grid >
+                    <FullCalendar
+                        plugins={[timeGridPlugin]}
+                        initialView="timeGridDay"
+                        weekends={true}
+                        editable={true}
+                        eventClick={(clickInfo) => {
+                            if (
+                                window.confirm(`このイベント「${clickInfo.event.title}」を削除しますか`)
+                            ) {
+
+                                removeEvent(clickInfo.event.title, clickInfo.event.id)
+                                clickInfo.event.remove();
+                            }
+                        }}
+                        events={dayEvents as EventSourceInput}
+                        locales={allLocales}
+                        locale="ja"
+                        contentHeight={'700px'}
+                    />
+                </Grid>
+
+
+            </Grid >
+
+        </>
+    );
+}
+
+export default DayCalender;
--- src/DayDataEditPage/dayEditApp.tsx
+++ src/DayDataEditPage/dayEditApp.tsx
@@ -1,169 +1,26 @@
-import React from "react";
-import { FilledInput, TextField } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from "react";
 import axios from 'axios';
 
+import DayCalender from "./dayCalender";
 
-export type DayData = {
-    userId: string,     //ex) "1234"
-    day: string,        //ex )"2022/06/07"
-    content: EventData[],
-}
-
-export type EventData = {
-    eventName: string,          //ex) "寝る"
-    eventStart: string,         //ex) "14:00"
-    eventEnd: string,           //ex) "16:00"
-    eventDescription: string,   //ex) "起床"
-}
+import { exampleDayDataType } from "../ExampleData/ExampleDataType";
 
 
-const SendDate = (data: DayData) => {
+const SendData = (data: exampleDayDataType) => {
     axios.post(`http://localhost:8000`, { data })
         .then(res => {
             console.log(res)
         })
 }
-type Prop = {
-    daydata: DayData | undefined,
-}
-const EventDataField: React.FC<Prop> = ({ daydata }) => {
-    console.log(daydata);
-    return (<div></div>);
-}
-
-const MyDatePicker = () => {
-    const value = new Date();
-    const handleChange = () => { };
-
-    if (1) {
-        return (
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DesktopDatePicker
-                    label="Date desktop"
-                    inputFormat="MM/dd/yyyy"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-
-                />
-            </LocalizationProvider>
-        );
-
-    } else {
-        return (
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <MobileDatePicker
-                    label="Date mobile"
-                    inputFormat="MM/dd/yyyy"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-
-                />
-            </LocalizationProvider>
-        );
-    }
-}
 
 const DayEditApp = () => {
-    const [startTime, setStartTime] = React.useState<Date | null>(new Date());
-    const [endTime, setEndTime] = React.useState<Date | null>(new Date());
-    const [text, setText] = React.useState("");
-    const [description, setDescription] = React.useState("");
-    const [dayData, setDayData] = React.useState<DayData>();
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setText(event.target.value);
-    };
-    const handleChangeDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDescription(event.target.value);
-    };
-
-    const addItems = () => {
-        const test = { userId: "", day: "", content: [{ eventName: "", eventStart: "", eventEnd: "", eventDescription: "" }] };
-        console.log(test.userId)
-        setDayData(test)
-    }
-
     return (
         <div>
-            <Stack spacing={4}>
+            <div>Edit Page</div>
+            <DayCalender
 
-                <div>Edit Page</div>
+            />
 
-                <MyDatePicker></MyDatePicker>
-                <TextField
-                    id="do-something"
-                    label="What did you do?"
-                    value={text}
-                    onChange={handleChange}
-                />
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <TimePicker
-                        ampmInClock
-                        views={['hours', 'minutes']}
-                        inputFormat="hh:mm"
-                        mask="__:__"
-                        label="Start Time (Hours and Minutes)"
-                        value={startTime}
-                        onChange={(newTime) => {
-                            setStartTime(newTime);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                    <TimePicker
-                        ampmInClock
-                        views={['hours', 'minutes']}
-                        inputFormat="hh:mm"
-                        mask="__:__"
-                        label="End Time (Hours and Minutes)"
-                        value={endTime}
-                        onChange={(newTime) => {
-                            setEndTime(newTime);
-                        }}
-                        minTime={startTime}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
-
-                <TextField
-                    id="time"
-                    label="Alarm clock"
-                    type="time"
-                    defaultValue="07:30"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    inputProps={{
-                        step: 300, // 5 min
-                    }}
-                />
-                <TextField
-                    id="do-something"
-                    label="How was it?"
-                    multiline
-                    rows={4}
-                    value={description}
-                    onChange={handleChangeDescription}
-                />
-                <Button variant="contained" disableElevation endIcon={<AddIcon />} onClick={() => addItems()}>
-                    Add
-                </Button>
-                <EventDataField daydata={dayData}></EventDataField>
-                <Button variant="contained" disableElevation endIcon={<SendIcon />}>
-                    Submit
-                </Button>
-
-            </Stack>
         </div>
     )
 }
