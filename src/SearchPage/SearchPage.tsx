643322c3eb6a026206403010186342a362bea17f
ryuki8643 <garyuki88me@gmail.com>
2022-06-23 20:58:31 +0900

    Add:Custom Search

--- src/SearchPage/SearchPage.tsx
+++ src/SearchPage/SearchPage.tsx
@@ -1,5 +1,5 @@
-import {NavigateFunction, useLocation, useNavigate} from "react-router-dom";
-import {Box, CardContent, Container, Grid, Paper, TextField} from "@mui/material";
+import { useLocation, useNavigate} from "react-router-dom";
+import {Box, Button, Container, Grid, Paper, TextField} from "@mui/material";
 import {exampleSearchDataType} from "../ExampleData/ExampleDataType";
 import {useEffect, useState} from "react";
 
@@ -8,7 +8,7 @@ export const SearchPage=()=>{
     const locationState=useLocation().state as {searchInput:string,SearchData:exampleSearchDataType}
     const autoCompleteObject={} as {[key:number]:{user:string,text:string,company:string,age:number,role:string}}
     locationState.SearchData["UserData"].forEach((value,index)=>{
-        console.log(value)
+        
         if (value) {
             autoCompleteObject[index] = {
                     user: value.UserId,
@@ -22,18 +22,19 @@ export const SearchPage=()=>{
     })
     const [companyInput,setCompanyInput]=useState("")
     const [roleInput,setRoleInput]=useState("")
-    const [ageDownInput,setAgeInput]=useState("")
-    const [ageUpInput,setUpAgeInput]=useState("")
-
+    const [ageDownInput,setDownAgeInput]=useState(0)
+    const [ageUpInput,setUpAgeInput]=useState(10000)
+    const [customSearch,setCustomSearch]=useState(false)
+    const [EnterKey,setEnterKey]=useState(false)
     const [searchInputCustom,setSearchInputCustom]=useState(locationState.searchInput)
 
 
     const navigate=useNavigate()
-    const filteredData=Object.values(autoCompleteObject).filter((value,index)=>{
+    const [filteredData,setFilteredData]=useState(Object.values(autoCompleteObject).filter((value,index)=>{
         return value.text.toLowerCase().indexOf(searchInputCustom.toLowerCase()) > -1
 
 
-    })
+    }) )
     const goToHomeWithUser=(user:string)=>{
 
         navigate("/",{
@@ -49,7 +50,34 @@ export const SearchPage=()=>{
         setSearchInputCustom(locationState.searchInput)
     },[locationState.searchInput])
 
-    console.log(filteredData,autoCompleteObject)
+    useEffect(()=>{
+        if(customSearch){
+            setFilteredData(Object.values(autoCompleteObject).filter((value,index)=>{
+
+                return ((value.company.toLowerCase().indexOf(companyInput) > -1) &&
+                    (value.role.toLowerCase().indexOf(roleInput)> -1) &&
+                    (value.age > ageDownInput) && (value.age < ageUpInput)
+
+                )
+
+
+            }) )
+        }
+        setCustomSearch(false)
+    },[customSearch])
+    useEffect(()=>{
+        if (EnterKey){
+            setCustomSearch(true)
+            setEnterKey(false)
+        }
+    },[EnterKey])
+    useEffect(()=>{
+        if(ageUpInput===0){
+            setUpAgeInput(10000)
+        }
+    },[ageUpInput])
+
+
     return (
         <Container maxWidth="xl" sx={{marginTop:2}}>
 
@@ -59,15 +87,42 @@ export const SearchPage=()=>{
                     <Grid item xs={12} key={"a"}>
                         <Paper>
 
-                            <TextField sx={{width:"25%",margin:"2%"}} label={"企業"}>企業</TextField>
-                            <TextField sx={{width:"25%",margin:"2%"}}　label={"役職"}>役職</TextField>
-                            <TextField　type={"number"} sx={{width:"17%",margin:"2%"}}　label={"年齢下限"}>年齢</TextField>
-                            <TextField　type={"number"} sx={{width:"17%",margin:"2%"}}label={"年齢上限"}>年齢</TextField>
-                    </Paper>
+                            <Box>
+
+                                <TextField sx={{width:"25%",margin:"2%"}} label={"企業"} onChange={(event)=>setCompanyInput(event.target.value)} onKeyPress={event => {
+                                    if (event.key==="Enter"){
+                                        setEnterKey(true)
+                                    }
+
+                                }}>企業</TextField>
+                                <TextField sx={{width:"25%",margin:"2%"}}　label={"役職"} onChange={(event)=>setRoleInput(event.target.value)} onKeyPress={event => {
+                                    if (event.key==="Enter"){
+                                        setEnterKey(true)
+                                    }
+
+                                }}>役職</TextField>
+                                <TextField　type={"number"} sx={{width:"17%",margin:"2%"}}　label={"年齢下限"} onChange={(event)=>setDownAgeInput(Number(event.target.value))} onKeyPress={event => {
+                                    if (event.key==="Enter"){
+                                        setEnterKey(true)
+                                    }
+
+                                }}>年齢</TextField>
+                                <TextField　type={"number"} sx={{width:"17%",margin:"2%"}}label={"年齢上限"} onChange={(event)=>setUpAgeInput(Number(event.target.value))} onKeyPress={event => {
+                                    if (event.key==="Enter"){
+                                        setEnterKey(true)
+                                    }
+
+                                }}>年齢</TextField>
+                            </Box>
+                            <Box　sx={{display:"flex"}}>
+                                <Button sx={{width:"25%",margin:"2%",flexGrow:1}} variant={"contained"} onClick={()=>setCustomSearch(true)}>詳細検索</Button>
+
+                            </Box>
+                        </Paper>
                     </Grid>
 
             {(filteredData).map((value,index)=>{
-                console.log(value,index);
+
                 return (
                     <Grid item xs={4} key={value.company+locationState.SearchData["UserData"][index].role+value.age}>
                         <Paper elevation={4} onClick={()=>goToHomeWithUser(value.user)}>
